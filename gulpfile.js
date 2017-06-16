20c9c9bd6a359b17e60765577db2c6aef5b759ad
Michael Ivonin <michael.ivonin@gmail.com>
2017-06-16 19:13:28 +0600

    Footer done

--- gulpfile.js
+++ gulpfile.js
@@ -102,12 +102,12 @@ gulp.task('clean', function() {
 // Img min
 gulp.task('img', function() {
   return gulp.src('./app/img/**/*')
-    .pipe(cache(imagemin({
+    /*.pipe(cache(imagemin({
       interlaced: true,
       progressive: true,
       svgoPlugins: [{removeViewBox: false}],
       use: [pngquant()]
-    })))
+    })))*/
     .pipe(gulp.dest('./dist/img'));
 });
 
