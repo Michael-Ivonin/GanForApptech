b3b4cbbb1762219330eaae3848c6e843fba0c118
Michael Ivonin <michael.ivonin@gmail.com>
2017-06-19 13:52:48 +0600

    Доработки

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+/**
+ * Created by IMike on 19.06.2017.
+ */
+
+'use strict';
+
+$(document).ready(function() {
+  // Hide or show main heading
+  /*$('.navbar-toggle').click(function() {
+    if ($('.greeting__presentation').is(':visible')) {
+      $('.greeting__presentation').hide();
+    } else {
+      $('.greeting__presentation').show();
+    }
+  });*/
+});
